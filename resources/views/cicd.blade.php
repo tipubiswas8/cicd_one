c2f38cc0e1f824461037ef23f7914fea0f6190ce
Md. Sohel Rana Tipu <tipumahmud8@gmail.com>
2026-08-03 18:28:30 +0600

    11

--- resources/views/cicd.blade.php
+++ resources/views/cicd.blade.php
@@ -10,17 +10,7 @@
 
 <body>
     <p>CI/CD Test</p>
-    <p>Work at first stage</p>
-    <p>Work at second stage</p>
-    <p>Work at third stage</p>
-    <p>Work at fourth stage</p>
-    <p>Work at fifth stage</p>
-    <p>Work at sixth stage</p>
-    <p>Work at seventh stage</p>
-    <p>Work at stage 9</p>
-    <p>Work at stage 10</p>
-    <p>Work at stage 11</p>
-    <p>Work at stage FINAL</p>
+    <p>1111</p>
 </body>
 
 </html>
